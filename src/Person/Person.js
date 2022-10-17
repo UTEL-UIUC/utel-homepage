993d17d4c576820d9f4e88854653c414f3417e4d
lewis500 <lewis500@gmail.com>
2022-10-16 22:35:49 -0500

    updated redicts

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 export default ({person}) => {
-  // let { person } = useParams();
-  return <div>{person}</div>;
+  return (<div>{person}</div>);
 };
